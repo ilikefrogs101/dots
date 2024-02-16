8c56fae1f53bfa7c36b20d9ed349fc38c9ab2b7d
ilikefrogs101 <87593516+ilikefrogs101@users.noreply.github.com>
2024-02-16 17:21:32 +1100

    fixed home file issues

--- hosts/someone/home/system/sway.nix
+++ hosts/someone/home/system/sway.nix
@@ -3,7 +3,8 @@
   programs.sway.enable = true;
   programs.sway.wrapperFeatures.gtk = true;
 
-    home-manager.users.ilikefrogs101.home.file = {
+  home-manager.extraSpecialArgs = { inherit inputs; };
+  home-manager.users.ilikefrogs101.home.file = {
     ".config/sway/config" = { source = config.lib.file.mkOutOfStoreSymlink ../../linked-dots/sway/config; target = ".config/sway/config"; };
   };
 }
